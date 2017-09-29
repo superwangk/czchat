356afc4f803fba1b5c3cac329bb20977dd83e27f
wangkun <1830990462@qq.com>
2017-09-29 12:05:28 +0800

    cz

---
+++ chat/index.js
@@ -0,0 +1,36 @@
+var czchat = angular.module("czchat", ["RongWebIMWidget"]);
+
+czchat.controller("main", ["$scope", "$http", "RongCustomerService", function($scope, $http, RongCustomerService) {
+
+    RongCustomerService.init({
+        appkey: "k51hidwqkngkb",
+        token: "+PTNc9OATs5o0A4eUD3z8gGYfOTYatRu0qStbcitAggTFM99gn81m2Cke6s5PRs7pcUoyCNVCH0IBtNhVAmlOA==",
+        customerServiceId: "KEFU150596993254445",
+        reminder: "在线咨询",
+        position: RongCustomerService.Position.right,
+        displayConversationList:true,
+        style: {
+            width: 500,
+            height:600
+        },
+        onSuccess: function(e) {
+            console.log('连接成功');
+        },
+        onError: function(e) {
+            console.log('连接失败');
+        }
+    });
+
+    $scope.show = function() {
+        RongCustomerService.show();
+    };
+
+    $scope.hidden = function() {
+        RongCustomerService.hidden();
+    };
+
+    $scope.del = function(){
+
+    }
+
+}]);
